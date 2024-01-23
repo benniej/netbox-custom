d94262b0bf1cebdff510eb33d07330845dc3858e
Bennie Joubert <benniej@jennyinternet.co.za>
2024-01-23 14:26:59 +0200

    Add a couple of more Jenny CS checks

--- reports/devices.py
+++ reports/devices.py
@@ -1,10 +1,28 @@
+import re
+
 from extras.reports import Report
 from dcim.models import Device
 
-class JennyCSLocalContextReport(Report):
-    description = "Check if devices with 'jenny-cs' tag contain a local context"
+class JENNY_Cutomer_Sector(Report):
+    description = "Check various attributes on a Jenny Customer Sector"
 
-    def test_cs_data(self):
+    def test_JCS_device_primary_IP(self):
+
+        for device in Device.objects.filter(tags="2"):
+            if device.primary_ip:
+                self.log_success(device)
+            else:
+                self.log_failure(device, "Check device primary IP")
+
+    def test_JCS_device_naming(self):
+
+        for device in Device.objects.filter(tags="2"):
+            if re.match("^J-[A-Z]{3}-CZ2[0-5][0-9]-(UBT|LTU|WAV|MIK|MIM|CAM|RAD)-[5-9][0-9]\.[0-9]{0,3}(-[a-zA-Z0-9_-]*)?", str(device.name)):
+                self.log_success(device)
+            else:
+                self.log_failure(device, "Check device name")
+
+    def test_JCS_wireless_details_exist(self):
 
         for device in Device.objects.filter(tags="2"):
             if device.local_context_data:

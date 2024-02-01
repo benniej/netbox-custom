87409b3defe270172b8c2d6207055b9b8fa360d5
Bennie Joubert <benniej@jennyinternet.co.za>
2024-02-01 10:49:26 +0200

    Add OLT to device name testing and rename Customer Sector to Radio Sector

--- reports/devices.py
+++ reports/devices.py
@@ -3,10 +3,10 @@ import re
 from extras.reports import Report
 from dcim.models import Device
 
-class JENNY_Cutomer_Sector(Report):
+class JENNY_Radio_Sector(Report):
     description = "Check various attributes on a Jenny Customer Sector"
 
-    def test_JCS_device_primary_IP(self):
+    def test_JRS_device_primary_IP(self):
 
         for device in Device.objects.filter(tags="2"):
             if device.primary_ip:
@@ -14,15 +14,15 @@ class JENNY_Cutomer_Sector(Report):
             else:
                 self.log_failure(device, "Check device primary IP")
 
-    def test_JCS_device_naming(self):
+    def test_JRS_device_naming(self):
 
         for device in Device.objects.filter(tags="2"):
-            if re.match("^J-[A-Z0-9]{3}-CZ2[0-5][0-9]-(?:UBT|LTU|WAV|MIK|MIM|CAM|RAD)-[5-9][0-9]\.[0-9]{0,3}(?:-[a-zA-Z0-9_-]*)?", str(device.name)):
+            if re.match("^J-[A-Z0-9]{3}-CZ2[0-5][0-9]-(?:UBT|LTU|WAV|MIK|MIM|CAM|RAD|OLT)-[5-9][0-9]\.[0-9]{0,3}(?:-[a-zA-Z0-9_-]*)?", str(device.name)):
                 self.log_success(device)
             else:
                 self.log_failure(device, "Check device name")
 
-    def test_JCS_wireless_details_exist(self):
+    def test_JRS_wireless_details_exist(self):
 
         for device in Device.objects.filter(tags="2"):
             if device.local_context_data:
